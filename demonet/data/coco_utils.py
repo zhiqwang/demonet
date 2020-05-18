0f3e3357d6a2e5bb3043f71df495747af3c87129
zhiqwang <zhiqwang@outlook.com>
2020-05-18 14:15:36 -0400

    Add voc datasets utils

--- demonet/data/coco_utils.py
+++ demonet/data/coco_utils.py
@@ -1,4 +1,7 @@
+import os
 import copy
+import numpy as np
+import cv2
 
 import torch
 import torch.utils.data
@@ -6,6 +9,82 @@ import torchvision
 
 from pycocotools import mask as coco_mask
 from pycocotools.coco import COCO
+
+
+class ConvertCocoPolysToMask(object):
+    def __call__(self, image, target):
+        height, width = image.shape[:2]
+
+        image_id = target["image_id"]
+        image_id = np.array([image_id])
+        image_shape = target["image_shape"]
+
+        anno = target["annotations"]
+
+        anno = [obj for obj in anno if obj['iscrowd'] == 0]
+
+        boxes = [obj["bbox"] for obj in anno]
+        # guard against no boxes via resizing
+        boxes = np.asarray(boxes, dtype=np.float32).reshape(-1, 4)
+        # BoxMode: convert from XYWH_ABS to XYXY_ABS
+        boxes[:, 2:] += boxes[:, :2]
+        boxes[:, 0::2] = np.clip(boxes[:, 0::2], 0, width)
+        boxes[:, 1::2] = np.clip(boxes[:, 1::2], 0, height)
+
+        classes = [obj["category_id"] for obj in anno]
+        classes = np.array(classes, dtype=np.int64)
+
+        masks = None
+        if len(anno[0]["segmentation"]) > 0:
+            segmentations = [obj["segmentation"] for obj in anno]
+            masks = convert_coco_poly_to_mask(segmentations, height, width)
+
+        keep = (boxes[:, 3] > boxes[:, 1]) & (boxes[:, 2] > boxes[:, 0])
+        boxes = boxes[keep]
+        classes = classes[keep]
+        if masks is not None:
+            masks = masks[keep]
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = classes
+        target["image_id"] = image_id
+        target["image_shape"] = image_shape
+
+        return image, target
+
+
+class CocoDetection(torchvision.datasets.CocoDetection):
+    def __init__(self, img_folder, ann_file, transforms):
+        super().__init__(img_folder, ann_file)
+        self._transforms = transforms
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: Tuple (image, target). target is the dict containing
+                object returned by ``coco.loadAnns``.
+        """
+        coco = self.coco
+        img_id = self.ids[index]
+        ann_ids = coco.getAnnIds(imgIds=img_id)
+        anns = coco.loadAnns(ann_ids)
+
+        path = coco.loadImgs(img_id)[0]['file_name']
+        image = cv2.imread(os.path.join(self.root, path))
+        image_shape = image.shape[1::-1]
+
+        target = dict(
+            image_id=img_id,
+            image_shape=image_shape,
+            annotations=anns,
+        )
+        if self._transforms is not None:
+            image, target = self._transforms(image, target)
+
+        return image, target
 
 
 class FilterAndRemapCocoCategories(object):
--- demonet/data/dataset.py
+++ demonet/data/dataset.py
@@ -1,47 +1,14 @@
 import os
-import numpy as np
-import cv2
+
 import torch
-import torchvision
 
 from .coco_utils import (
     _coco_remove_images_without_annotations,
-    convert_coco_poly_to_mask,
+    CocoDetection,
+    ConvertCocoPolysToMask,
 )
+from .voc_utils import VOCDetection, ConvertVOCtoCOCO
 from . import transforms as T
-
-
-class CocoDetection(torchvision.datasets.CocoDetection):
-    def __init__(self, img_folder, ann_file, transforms):
-        super().__init__(img_folder, ann_file)
-        self._transforms = transforms
-
-    def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
-        Returns:
-            tuple: Tuple (image, target). target is the dict containing
-                object returned by ``coco.loadAnns``.
-        """
-        coco = self.coco
-        img_id = self.ids[index]
-        ann_ids = coco.getAnnIds(imgIds=img_id)
-        anns = coco.loadAnns(ann_ids)
-
-        path = coco.loadImgs(img_id)[0]['file_name']
-        image = cv2.imread(os.path.join(self.root, path))
-        image_shape = image.shape[1::-1]
-
-        target = dict(
-            image_id=img_id,
-            image_shape=image_shape,
-            annotations=anns,
-        )
-        if self._transforms is not None:
-            image, target = self._transforms(image, target)
-
-        return image, target
 
 
 def get_coco(
@@ -77,6 +44,27 @@ def get_coco(
         dataset = _coco_remove_images_without_annotations(dataset)
 
     # dataset = torch.utils.data.Subset(dataset, [i for i in range(500)])
+
+    return dataset
+
+
+def get_voc(
+    root,
+    image_set,
+    transforms,
+):
+    t = [ConvertVOCtoCOCO()]
+
+    if transforms is not None:
+        t.append(transforms)
+    transforms = T.Compose(t)
+
+    dataset = VOCDetection(
+        img_folder=root,
+        year='2007',
+        image_set=image_set,
+        transforms=transforms,
+    )
 
     return dataset
 
@@ -145,49 +133,6 @@ def collate_eval(batch):
     images = torch.stack(images, 0)
 
     return images, targets
-
-
-class ConvertCocoPolysToMask(object):
-    def __call__(self, image, target):
-        height, width = image.shape[:2]
-
-        image_id = target["image_id"]
-        image_id = np.array([image_id])
-        image_shape = target["image_shape"]
-
-        anno = target["annotations"]
-
-        anno = [obj for obj in anno if obj['iscrowd'] == 0]
-
-        boxes = [obj["bbox"] for obj in anno]
-        # guard against no boxes via resizing
-        boxes = np.asarray(boxes, dtype=np.float32).reshape(-1, 4)
-        # BoxMode: convert from XYWH_ABS to XYXY_ABS
-        boxes[:, 2:] += boxes[:, :2]
-        boxes[:, 0::2] = np.clip(boxes[:, 0::2], 0, width)
-        boxes[:, 1::2] = np.clip(boxes[:, 1::2], 0, height)
-
-        classes = [obj["category_id"] for obj in anno]
-        classes = np.array(classes, dtype=np.int64)
-
-        masks = None
-        if len(anno[0]["segmentation"]) > 0:
-            segmentations = [obj["segmentation"] for obj in anno]
-            masks = convert_coco_poly_to_mask(segmentations, height, width)
-
-        keep = (boxes[:, 3] > boxes[:, 1]) & (boxes[:, 2] > boxes[:, 0])
-        boxes = boxes[keep]
-        classes = classes[keep]
-        if masks is not None:
-            masks = masks[keep]
-
-        target = {}
-        target["boxes"] = boxes
-        target["labels"] = classes
-        target["image_id"] = image_id
-        target["image_shape"] = image_shape
-
-        return image, target
 
 
 class Compose(object):
