297dabf8c91368c77a2643fc5fb54d4b905edd27
zhiqwang <zhiqwang@outlook.com>
2020-05-29 05:37:56 -0400

    Add pelee object detection network

---
+++ demonet/modeling/backbone/peleenet.py
@@ -0,0 +1,220 @@
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchvision.models.utils import load_state_dict_from_url
+
+
+model_urls = {
+    'peleenet_v1': './models/peleenetv1-a639ec97.pth',
+}
+
+
+class BasicConv2d(nn.Module):
+
+    def __init__(self, in_channels, out_channels, activation=True, **kwargs):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.norm = nn.BatchNorm2d(out_channels)
+        self.activation = activation
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.norm(x)
+        if self.activation:
+            return F.relu(x, inplace=True)
+        else:
+            return x
+
+
+class _DenseLayer(nn.Module):
+    def __init__(
+        self, num_input_features, growth_rate, bn_size, drop_rate,
+    ):
+        super().__init__()
+
+        growth_rate = int(growth_rate / 2)
+        inter_channel = int(growth_rate * bn_size / 4) * 4
+
+        if inter_channel > num_input_features / 2:
+            inter_channel = int(num_input_features / 8) * 4
+
+        self.branch1a = BasicConv2d(num_input_features, inter_channel, kernel_size=1)
+        self.branch1b = BasicConv2d(inter_channel, growth_rate, kernel_size=3, padding=1)
+
+        self.branch2a = BasicConv2d(num_input_features, inter_channel, kernel_size=1)
+        self.branch2b = BasicConv2d(inter_channel, growth_rate, kernel_size=3, padding=1)
+        self.branch2c = BasicConv2d(growth_rate, growth_rate, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        branch1 = self.branch1a(x)
+        branch1 = self.branch1b(branch1)
+
+        branch2 = self.branch2a(x)
+        branch2 = self.branch2b(branch2)
+        branch2 = self.branch2c(branch2)
+        out = torch.cat([x, branch1, branch2], 1)
+
+        return out
+
+
+class _DenseBlock(nn.Sequential):
+    _version = 2
+
+    def __init__(
+        self, num_layers, num_input_features, bn_size, growth_rate, drop_rate,
+    ):
+        super().__init__()
+        for i in range(num_layers):
+            layer = _DenseLayer(
+                num_input_features + i * growth_rate,
+                growth_rate,
+                bn_size,
+                drop_rate,
+            )
+            self.add_module('denselayer%d' % (i + 1), layer)
+
+
+class _StemBlock(nn.Module):
+
+    def __init__(self, num_input_channels, num_init_features):
+        super().__init__()
+
+        num_stem_features = int(num_init_features / 2)
+
+        self.stem1 = BasicConv2d(
+            num_input_channels, num_init_features, kernel_size=3,
+            stride=2, padding=1,
+        )
+        self.stem2a = BasicConv2d(
+            num_init_features, num_stem_features, kernel_size=1,
+        )
+        self.stem2b = BasicConv2d(
+            num_stem_features, num_init_features, kernel_size=3,
+            stride=2, padding=1,
+        )
+        self.stem3 = BasicConv2d(
+            2 * num_init_features, num_init_features, kernel_size=1,
+        )
+        self.pool = nn.MaxPool2d(
+            kernel_size=2, stride=2, ceil_mode=True,
+        )
+
+    def forward(self, x):
+        out = self.stem1(x)
+
+        branch2 = self.stem2a(out)
+        branch2 = self.stem2b(branch2)
+        branch1 = self.pool(out)
+
+        out = torch.cat([branch1, branch2], dim=1)
+        out = self.stem3(out)
+
+        return out
+
+
+class PeleeNet(nn.Module):
+    r"""PeleeNet model class, based on
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>` and
+    `"Pelee: A Real-Time Object Detection System on Mobile Devices" <https://arxiv.org/pdf/1804.06882.pdf>`
+    Args:
+        growth_rate (int) - how many filters to add each layer (`k` in paper)
+        block_config (list of 4 ints) - how many layers in each pooling block
+        num_init_features (int) - the number of filters to learn in the first convolution layer
+        bn_size (list of 4 ints) - multiplicative factor for number of bottle neck layers
+          (i.e. bn_size * k features in the bottleneck layer)
+        drop_rate (float) - dropout rate after each dense layer
+        num_classes (int) - number of classification classes
+        memory_efficient (bool) - If True, uses checkpointing. Much more memory efficient,
+          but slower. Default: *False*. See `"paper" <https://arxiv.org/pdf/1707.06990.pdf>`_
+    """
+
+    def __init__(
+        self,
+        growth_rate=32,
+        block_config=(3, 4, 8, 6),
+        num_init_features=32,
+        bn_size=(1, 2, 4, 4),
+        drop_rate=0.05,
+        num_classes=1000,
+        memory_efficient=False,
+    ):
+        super().__init__()
+
+        self.features = nn.Sequential(OrderedDict([
+            ('stemblock', _StemBlock(3, num_init_features)),
+        ]))
+
+        # Each denseblock
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(
+                num_layers=num_layers,
+                num_input_features=num_features,
+                bn_size=bn_size[i],
+                growth_rate=growth_rate,
+                drop_rate=drop_rate,
+            )
+            self.features.add_module('denseblock%d' % (i + 1), block)
+            num_features = num_features + num_layers * growth_rate
+
+            trans = BasicConv2d(num_features, num_features, kernel_size=1, stride=1, padding=0)
+            self.features.add_module('transition%d' % (i + 1), trans)
+
+            if i != len(block_config) - 1:
+                trans_pool = nn.AvgPool2d(kernel_size=2, stride=2, ceil_mode=True)
+                self.features.add_module('transition%d_pool' % (i + 1), trans_pool)
+                num_features = num_features
+
+        # Linear layer
+        self.classifier = nn.Linear(num_features, num_classes)
+        self.drop_rate = drop_rate
+
+        # Official init from torch repo.
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        features = self.features(x)
+        out = F.adaptive_avg_pool2d(features, (1, 1))
+        out = torch.flatten(out, 1)
+        if self.drop_rate > 0:
+            out = F.dropout(out, p=self.drop_rate, training=self.training)
+        out = self.classifier(out)
+        return out
+
+
+def _peleenet(
+    arch, growth_rate, block_config, num_init_features, bn_size,
+    pretrained, progress, **kwargs,
+):
+    model = PeleeNet(growth_rate, block_config, num_init_features, **kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls[arch], progress)
+        model.load_state_dict(state_dict)
+    return model
+
+
+def peleenet_v1(pretrained=False, progress=True, **kwargs):
+    r"""PeleeNet model class, based on
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>` and
+    `"Pelee: A Real-Time Object Detection System on Mobile Devices" <https://arxiv.org/pdf/1804.06882.pdf>`
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+        memory_efficient (bool) - If True, uses checkpointing. Much more memory efficient,
+          but slower. Default: *False*. See `"paper" <https://arxiv.org/pdf/1707.06990.pdf>`_
+    """
+    return _peleenet(
+        'peleenet_v1', 32, (3, 4, 8, 6), 32, (1, 2, 4, 4),
+        pretrained, progress, **kwargs,
+    )
---
+++ demonet/modeling/pelee.py
@@ -0,0 +1,220 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .backbone.backbone_utils import Backbone
+from .backbone.peleenet import BasicConv2d, peleenet_v1
+from .box_heads.multibox_head import MultiBoxHeads
+
+
+class Pelee(nn.Module):
+    r"""Pelee model class, based on
+    `"Pelee: A Real-Time Object Detection System on Mobile Devices" <https://arxiv.org/pdf/1804.06882.pdf>`
+    Args:
+        body: PeleeNet body layers
+        extras: extra layers
+        resblock: res block layers that feed to multibox loc and conf layers
+        head: "multibox head" consists of loc and conf conv layers
+    """
+    def __init__(self, body, extras, resblock, head, **kwargs):
+        super().__init__()
+
+        self.features = body.features
+
+        self.extras = nn.ModuleList(extras)
+        self.resblock = nn.ModuleList(resblock)
+
+        self.loc_conv = nn.ModuleList(head[0])
+        self.conf_conv = nn.ModuleList(head[1])
+
+        self.multibox_heads = MultiBoxHeads(**kwargs)
+
+    def eager_outputs(self, losses, detections):
+        if self.training:
+            return losses
+
+        return detections
+
+    def forward(self, images, targets=None):
+        """
+        Arguments:
+            images (Tensor): images to be processed
+            targets (Tensor): ground-truth boxes present in the image (optional)
+        Returns:
+            result (list[BoxList] or dict[Tensor]): the output from the model.
+                During training, it returns a dict[Tensor] which contains the losses.
+                During testing, it returns list[BoxList] contains additional fields
+                like `scores`, `labels` and `mask` (for Mask R-CNN models).
+        """
+        sources = list()
+        loc = list()
+        conf = list()
+
+        for k, feat in enumerate(self.features):
+            images = feat(images)
+            if k == 8:
+                sources.append(images)
+
+        sources.append(images)
+
+        for k, v in enumerate(self.extras):
+            images = v(images)
+            if k % 2 == 1:
+                sources.append(images)
+
+        for k, x in enumerate(sources):
+            sources[k] = self.resblock[k](x)
+
+        for (x, l, c) in zip(sources, self.loc_conv, self.conf_conv):
+            loc.append(l(x).permute(0, 2, 3, 1).contiguous())
+            conf.append(c(x).permute(0, 2, 3, 1).contiguous())
+
+        loc = torch.cat([o.view(o.size(0), -1) for o in loc], 1)
+        conf = torch.cat([o.view(o.size(0), -1) for o in conf], 1)
+
+        detections, detector_losses = self.multibox_heads(loc, conf, targets)
+
+        return self.eager_outputs(detector_losses, detections)
+
+
+def build_backbone(pretrained=True, trainable_layers=1):
+    """
+    Constructs a specified PeleeNet backbone. Freezes the specified number of layers in the backbone.
+
+    Arguments:
+        pretrained (bool): If True, returns a model with backbone pre-trained on Imagenet
+        trainable_layers (int): number of trainable (not frozen) peleenet layers starting from final block.
+    """
+    backbone = peleenet_v1(pretrained=pretrained)
+
+    # select layers that wont be frozen
+    assert trainable_layers == 1
+    layers_to_train = ['features']
+    # freeze layers only if pretrained backbone is used
+    for name, parameter in backbone.named_parameters():
+        if all([not name.startswith(layer) for layer in layers_to_train]):
+            parameter.requires_grad_(False)
+
+    return_layers = {'features': '0'}
+
+    model = Backbone(backbone, return_layers)
+
+    return model.body
+
+
+class ConvReLU(nn.Module):
+
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+
+    def forward(self, x):
+        out = F.relu(self.conv(x), inplace=True)
+        return out
+
+
+def build_extras(i, batch_norm=False):
+    layers = list()
+    in_channels = i
+    channels = [128, 256, 128, 256, 128, 256]
+    stride = [1, 2, 1, 1, 1, 1]
+    padding = [0, 1, 0, 0, 0, 0]
+
+    for k, v in enumerate(channels):
+        if k % 2 == 0:
+            if batch_norm:
+                layers.append(BasicConv2d(
+                    in_channels, v, kernel_size=1, padding=padding[k],
+                ))
+            else:
+                layers.append(ConvReLU(
+                    in_channels, v, kernel_size=1, padding=padding[k],
+                ))
+        else:
+            if batch_norm:
+                layers.append(BasicConv2d(
+                    in_channels, v, kernel_size=3, padding=padding[k],
+                    stride=stride[k],
+                ))
+            else:
+                layers.append(ConvReLU(
+                    in_channels, v, kernel_size=3, padding=padding[k],
+                    stride=stride[k],
+                ))
+        in_channels = v
+
+    return layers
+
+
+class ResBlock(nn.Module):
+
+    def __init__(self, in_channels):
+        super().__init__()
+        self.res1a = ConvReLU(in_channels, 128, kernel_size=1)
+        self.res1b = ConvReLU(128, 128, kernel_size=3, padding=1)
+        self.res1c = ConvReLU(128, 256, kernel_size=1)
+
+        self.res2a = ConvReLU(in_channels, 256, kernel_size=1)
+
+    def forward(self, x):
+        out1 = self.res1a(x)
+        out1 = self.res1b(out1)
+        out1 = self.res1c(out1)
+
+        out2 = self.res2a(x)
+        out = out1 + out2
+        return out
+
+
+def build_resblock(nchannels):
+    resblock_layers = list()
+    for k, v in enumerate(nchannels):
+        resblock_layers.append(ResBlock(v))
+    return resblock_layers
+
+
+def build_multibox(cfg, num_classes):
+    loc_layers = list()
+    conf_layers = list()
+
+    for k, v in enumerate([256] * 5):
+        loc_layers.append(nn.Conv2d(v, cfg[k] * 4, kernel_size=1))
+        conf_layers.append(nn.Conv2d(v, cfg[k] * num_classes, kernel_size=1))
+
+    return (loc_layers, conf_layers)
+
+
+model_urls = {'pelee': ''}
+
+
+def build(size=304, num_classes=21, **kwargs):
+    if size != 304:
+        raise NotImplementedError(
+            "You specified size [{}]. However, currently only "
+            "Pelee304 (size=304) is supported!".format(size),
+        )
+
+    pretrained_backbone = False
+    body_layers = build_backbone(pretrained_backbone)
+    extras_layers = build_extras(704, batch_norm=True)
+    nchannels = [512, 704, 256, 256, 256]
+    anchor_nms_cfg = [6, 6, 6, 6, 6]  # number of boxes per feature map location
+    resblock_layers = build_resblock(nchannels)
+    head_layers = build_multibox(anchor_nms_cfg, num_classes)
+
+    model = Pelee(
+        body_layers,
+        extras_layers,
+        resblock_layers,
+        head_layers,
+        image_size=size,
+        aspect_ratios=[[2, 3], [2, 3], [2, 3], [2, 3], [2, 3]],
+        feature_maps=[19, 10, 5, 3, 1],
+        min_ratio=15,
+        max_ratio=90,
+        steps=[16, 30, 60, 101, 304],
+        clip=True,
+        **kwargs,
+    )
+
+    return model
