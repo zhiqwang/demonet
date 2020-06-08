d53c21b0ec98a132b02202844d1426fd09728704
zhiqwang <zhiqwang@outlook.com>
2020-06-08 02:52:02 -0400

    Fix lint

--- demonet/data/__init__.py
+++ demonet/data/__init__.py
@@ -3,6 +3,7 @@ import torch.utils.data
 import torchvision
 
 from .coco import build as build_coco
+from .voc import build as build_voc
 
 
 def get_coco_api_from_dataset(dataset):
@@ -28,7 +29,6 @@ def build_dataset(image_set, dataset_year, args):
         if args.dataset_file == 'coco':
             dataset = build_coco(image_set, year, args)
         elif args.dataset_file == 'voc':
-            from .voc import build as build_voc
             dataset = build_voc(image_set, year, args)
         else:
             raise ValueError(f'dataset {args.dataset_file} not supported')
