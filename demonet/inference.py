6d129d3c04a66a61ef635e9620d9da2ca6bb69c6
zhiqwang <zhiqwang@outlook.com>
2020-06-07 23:54:03 -0400

    Minor fixes

--- demonet/inference.py
+++ demonet/inference.py
@@ -1,7 +1,7 @@
 import cv2
 import torch
 
-from .models.backbone.vgg import build_model
+from .models import build_model
 from .utils.image import image_transform
 from .utils.overlay import overlay_boxes, overlay_class_names
 
@@ -10,20 +10,18 @@ def main(args):
     print('>>> Args: {}'.format(args))
 
     device = torch.device('cpu')
-    model = build_model(
-        size=args.image_size,
-        num_classes=args.num_classes,
-    )
+    model = build_model(args)
     model.eval()
     model.to(device)
 
     checkpoint = torch.load(args.resume, map_location='cpu')
-    model.load_state_dict(checkpoint)
+    model.load_state_dict(checkpoint['model'])
 
     image = image_transform(
         args.image_path,
         input_shape=(300, 300),
-        mean=[104, 117, 124],  # BGR
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225],
     )
 
     image = image[None, :]
@@ -42,11 +40,11 @@ def main(args):
 def parse_args():
     import argparse
     parser = argparse.ArgumentParser(description='pytorch ssd converter')
-    parser.add_argument('--arch', default='ssd',
+    parser.add_argument('--arch', default='ssd_lite_mobilenet_v2',
                         help='model architecture: {} (default: ssd)')
-    parser.add_argument('--image-size', default=304, type=int,
+    parser.add_argument('--image-size', default=300, type=int,
                         help='input size of models')
-    parser.add_argument('--num-classes', default=4, type=int,
+    parser.add_argument('--num-classes', default=21, type=int,
                         help='number classes of datasets')
     parser.add_argument('--device', default='cpu',
                         help='device')
