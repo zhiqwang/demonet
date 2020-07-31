a2c71dba7b661813ca49776926b41c6cbef65a95
zhiqwang <zhiqwang@outlook.com>
2020-07-31 13:23:28 -0400

    Minor fixes and add label list of VOC datasets

--- export/detection.cpp
+++ export/detection.cpp
@@ -16,12 +16,15 @@
 #define kCHANNELS 3
 #define kTOP_K 3
 
-bool LoadImage(std::string file_name, cv::Mat &image) {
+bool LoadImage(std::string file_name, cv::Mat &image,
+               float &width, float &height) {
   image = cv::imread(file_name);  // CV_8UC3
   if (image.empty() || !image.data) {
     return false;
   }
   cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
+  width = float(image.cols);
+  height = float(image.rows);
   std::cout << "== image size: " << image.size() << " ==" << std::endl;
 
   // scale image to fit
@@ -72,13 +75,15 @@ int main(int argc, const char *argv[]) {
 
   std::string file_name = "";
   cv::Mat image;
+  float img_width = 0.;
+  float img_height = 0.;
   while (true) {
     std::cout << "== Input image path: [enter Q to exit]" << std::endl;
     std::cin >> file_name;
     if (file_name == "Q") {
       break;
     }
-    if (LoadImage(file_name, image)) {
+    if (LoadImage(file_name, image, img_width, img_height)) {
       auto input_tensor = torch::from_blob(
           image.data, {1, kIMAGE_SIZE, kIMAGE_SIZE, kCHANNELS});
       input_tensor = input_tensor.permute({0, 3, 1, 2});
@@ -86,23 +91,25 @@ int main(int argc, const char *argv[]) {
       input_tensor[0][1] = input_tensor[0][1].sub_(0.456).div_(0.224);
       input_tensor[0][2] = input_tensor[0][2].sub_(0.406).div_(0.225);
 
+      auto image_sizes = torch::tensor({img_width, img_height});
+
       // to GPU
       input_tensor = input_tensor.to(at::kCUDA);
+      image_sizes = image_sizes.to(at::kCUDA);
 
-      torch::Tensor out_tensor = module.forward({input_tensor}).toTensor();
+      auto results = module.forward({input_tensor, image_sizes}).toTensor();
 
-      auto results = out_tensor.sort(-1, true);
-      auto softmaxs = std::get<0>(results)[0].softmax(0);
-      auto indexs = std::get<1>(results)[0];
+      // at::Tensor detections = std::get<1>(results)[0];
+      std::cout << "Results: " << results << std::endl;
 
-      for (int i = 0; i < kTOP_K; ++i) {
-        auto idx = indexs[i].item<int>();
-        std::cout << "    ============= Top-" << i + 1
-                  << " =============" << std::endl;
-        std::cout << "    Label:  " << labels[idx] << std::endl;
-        std::cout << "    With Probability:  "
-                  << softmaxs[i].item<float>() * 100.0f << "%" << std::endl;
-      }
+      // for (int i = 0; i < kTOP_K; ++i) {
+      //   auto idx = indexs[i].item<int>();
+      //   std::cout << "    ============= Top-" << i + 1
+      //             << " =============" << std::endl;
+      //   std::cout << "    Label:  " << labels[idx] << std::endl;
+      //   std::cout << "    With Probability:  "
+      //             << softmaxs[i].item<float>() * 100.0f << "%" << std::endl;
+      // }
 
     } else {
       std::cout << "Can't load the image, please check your path." << std::endl;
