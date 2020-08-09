a3a789d419e05a9d65a7e8973da7324db086034a
zhiqwang <zhiqwang@outlook.com>
2020-08-08 23:12:01 -0400

    Minor fixes

--- util/image_utils.py
+++ util/image_utils.py
@@ -1,6 +1,49 @@
 import numpy as np
 import cv2
 
+from IPython import display
+from PIL import Image
+
+
+def load_image(
+    image_name,
+    input_shape=(304, 304),
+    mean=None,
+    std=None,
+):
+    image = cv2.imread(image_name)
+    image = image[:, :, ::-1]  # BGR to RGB
+    image = cv2.resize(image, input_shape, interpolation=cv2.INTER_CUBIC)
+    image = np.ascontiguousarray(image, dtype=np.float32)  # uint8 to float32
+    image /= 255.0  # 0 - 255 to 0.0 - 1.0
+    # Normalization
+    if mean is not None:
+        mean = np.array(mean, dtype=np.float32)  # RGB
+        image -= mean
+    if std is not None:
+        std = np.array(std, dtype=np.float32)  # RGB
+        image /= std
+    image = image.transpose([2, 0, 1])  # change to C x H x W
+
+    return image
+
+
+def cv2_imshow(a):
+    """A replacement for cv2.imshow() for use in Jupyter notebooks.
+    Args:
+        a : np.ndarray. shape (N, M) or (N, M, 1) is an NxM grayscale image. shape
+        (N, M, 3) is an NxM BGR color image. shape (N, M, 4) is an NxM BGRA color
+        image.
+    """
+    a = a.clip(0, 255).astype('uint8')
+    # cv2 stores colors as BGR; convert to RGB
+    if a.ndim == 3:
+        if a.shape[2] == 4:
+            a = cv2.cvtColor(a, cv2.COLOR_BGRA2RGBA)
+        else:
+            a = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
+    display.display(Image.fromarray(a))
+
 
 def select_top_predictions(predictions, threshold):
     idx = (predictions['scores'] > threshold).nonzero(as_tuple=False).squeeze(1)
